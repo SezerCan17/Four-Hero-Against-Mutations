d0b7b221afcb4344c2b2b7d62c3d94ddeb501093
Sezer Can <118104067+SezerCan17@users.noreply.github.com>
2023-02-17 21:57:36 +0300

    Mekanikler
    Idle,run,roll,jump,doubleJump,attack1,attack2,attack3
    z,x,c,a,space

---
+++ Deneme/Assets/Script/Player.cs
@@ -0,0 +1,207 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Animator myanims;
+
+    [SerializeField]
+    private float speed;
+    private bool rlook;
+    [SerializeField]
+    private Transform[] ContactPoint;
+    public float ContactDimater;
+    public LayerMask whichGround;
+
+    private bool inGround;
+    private bool jump;
+    private bool doubleJump;
+    [SerializeField]
+    private bool AirControll;
+    public float JumpPower;
+
+    private bool attack1;
+    private bool attack2;
+    private bool attack3;
+    private bool roll;
+    
+    void Start()
+    {
+        rlook= true;
+        rb2D= GetComponent<Rigidbody2D>();
+        myanims= GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        Controller();
+    }
+
+    private void FixedUpdate()
+    {
+        float hori = Input.GetAxis("Horizontal");
+        inGround = atGround();
+        BasicMovement(hori);
+        ChangeDirection(hori);
+        AttackMovement();
+        Reset_();
+        MovementLayer();
+    }
+
+
+    private void BasicMovement(float hori)
+    {
+        if(rb2D.velocity.y<0)
+        {
+            myanims.SetBool("Down", true);
+        }
+        if(!myanims.GetBool("Roll") && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack1") && (inGround ||AirControll))
+        {
+            rb2D.velocity = new Vector2(hori * speed, rb2D.velocity.y);
+        }
+        else if(!myanims.GetBool("Roll") && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack2") && (inGround || AirControll))
+        {
+            rb2D.velocity = new Vector2(hori * speed, rb2D.velocity.y);
+        }
+        else if (!myanims.GetBool("Roll") && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack3") && (inGround || AirControll))
+        {
+            rb2D.velocity = new Vector2(hori * speed, rb2D.velocity.y);
+        }
+
+        if ((inGround || doubleJump) && jump )
+        {
+            if(inGround)
+            {
+                doubleJump = true;
+            }
+            else
+            {
+                doubleJump=false; 
+            }
+            inGround= false;
+            rb2D.AddForce(new Vector2(0,JumpPower));
+            myanims.SetTrigger("Jump");
+        }
+        
+        if(roll && !this.myanims.GetCurrentAnimatorStateInfo(0).IsName("Roll"))
+        {
+            myanims.SetBool("Roll", true);
+            roll = false;
+        }
+        else if(!roll && !this.myanims.GetCurrentAnimatorStateInfo(0).IsName("Roll"))
+        {
+            myanims.SetBool("Roll", false);
+        }
+
+        myanims.SetFloat("Speed",Mathf.Abs(hori));
+    }
+
+    private void AttackMovement()
+    {
+        if(attack1 && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack1"))
+        {
+            myanims.SetTrigger("Attack1");
+            rb2D.velocity = Vector2.zero;
+        }
+        else if (attack2 && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack2"))
+        {
+            myanims.SetTrigger("Attack2");
+            rb2D.velocity = Vector2.zero;
+        }
+        else if (attack3 && !this.myanims.GetCurrentAnimatorStateInfo(0).IsTag("Attack3"))
+        {
+            myanims.SetTrigger("Attack3");
+            rb2D.velocity = Vector2.zero;
+        }
+
+    }
+
+    private void Controller()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)) 
+        {
+            attack1= true;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            attack2 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            attack3 = true;
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            roll = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump= true;
+
+           
+            
+            
+        }
+
+    }
+
+    private void ChangeDirection(float hori)
+    {
+        if(hori>0 && !rlook || hori<0 && rlook)
+        {
+            rlook = !rlook;
+            Vector3 yon = transform.localScale;
+            yon.x *= -1;
+            transform.localScale = yon;
+        }
+    }
+
+    private void Reset_()
+    {
+        attack1= false;
+        attack2= false;
+        attack3 = false;
+        roll = false;
+        jump= false;
+
+    }
+
+    private bool atGround()
+    {
+        if(rb2D.velocity.y<=0)
+        {
+            foreach(Transform nokta in ContactPoint)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(nokta.position, ContactDimater, whichGround);
+
+                for(int i=0;i<colliders.Length;i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        myanims.ResetTrigger("Jump");
+                        myanims.SetBool("Down", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void MovementLayer()
+    {
+        if(inGround)
+        {
+            myanims.SetLayerWeight(1, 1);
+
+        }
+        else
+        {
+            myanims.SetLayerWeight(1, 0);
+        }
+
+    }
+}
